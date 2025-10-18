9171236b795fcf0a1c1c1c9c517d37e70f2f727d
Safarik95 <116165352+Safarik95@users.noreply.github.com>
2025-10-18 14:06:07 +0300

    Update currency.go

--- internal/types/currency.go
+++ internal/types/currency.go
@@ -10,12 +10,3 @@ const (
 func (c Currency) String() string {
 	return string(c)
 }
-
-/*func (c Currency) IsValid() bool {
-	switch c {
-	case BTC, ETH:
-		return true
-	default:
-		return false
-	}
-}*/
